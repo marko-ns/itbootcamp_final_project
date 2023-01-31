52a39c42548d4ebc64abcefd20c41710a29147e7
marko-ns <marko96jovic@gmail.com>
2023-01-31 18:30:41 +0100

    Added LoginPage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,4 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public abstract class BasePage {
+
+    protected WebDriver webDriver;
+    protected WebDriverWait webDriverWait;
+
+    public BasePage(WebDriver driver, WebDriverWait driverWait) {
+        this.webDriver = driver;
+        this.webDriverWait = driverWait;
+        PageFactory.initElements(this.webDriver, this);
+    }
 }
---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver driver, WebDriverWait driverWait) {
+        super(driver, driverWait);
+    }
+
+    @FindBy(id = "email")
+    WebElement emailInput;
+
+    @FindBy(id = "password")
+    WebElement passwordInput;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div/div[1]")
+    WebElement errorMessage;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[3]/span/form/div/div[3]/button")
+    WebElement loginButton;
+
+    public void login (String email, String password){
+        emailInput.clear();
+        passwordInput.clear();
+
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+
+        loginButton.click();
+    }
+
+}

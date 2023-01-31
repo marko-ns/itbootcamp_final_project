514875e06dfb5f9b49054ed1bee05113c7702d98
marko-ns <marko96jovic@gmail.com>
2023-01-31 19:28:15 +0100

    Added HomePage

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage extends BasePage{
+
+    public HomePage(WebDriver driver, WebDriverWait driverWait) {
+        super(driver, driverWait);
+    }
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button")
+    private WebElement languageButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")
+    private WebElement headerText;
+
+}

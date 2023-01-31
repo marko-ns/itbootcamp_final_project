721ba60075302f1cbbc98d17aeec4144a2f179c2
marko-ns <marko96jovic@gmail.com>
2023-01-31 20:04:06 +0100

    Added BaseTest attributes

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,35 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+
+import java.time.Duration;
+
 public abstract class BaseTest {
+
+    protected WebDriver webDriver;
+    protected WebDriverWait webDriverWait;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void beforeClass(){
+        this.webDriver = new ChromeDriver();
+        this.webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(20));
+        this.homePage = new HomePage(webDriver, webDriverWait);
+    }
+
+    @AfterClass
+    public void afterClass(){
+        webDriver.quit();
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        webDriver.get("https://vue-demo.daniel-avellaneda.com/");
+    }
 }

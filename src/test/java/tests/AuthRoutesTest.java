4a3ac8b5ed4460ca149b1a6356dd39c17286874c
marko-ns <marko96jovic@gmail.com>
2023-02-02 15:42:11 +0100

    Added forbidsProfile test

--- src/test/java/tests/AuthRoutesTest.java
+++ src/test/java/tests/AuthRoutesTest.java
@@ -15,4 +15,13 @@ public class AuthRoutesTest extends BaseTest{
 
         Assert.assertEquals(webDriver.getCurrentUrl(), "https://vue-demo.daniel-avellaneda.com/login");
     }
+
+    @Test
+    public void forbidsProfile(){
+        webDriver.get("https://vue-demo.daniel-avellaneda.com/profile");
+        //presence of Login header
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[1]/h1")));
+
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://vue-demo.daniel-avellaneda.com/login");
+    }
 }
